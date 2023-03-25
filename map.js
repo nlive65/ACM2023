eb31f180ce5a33b42f30e1f3b0bf01b35d8d6d66
Nick <nbm0012@uah.edu>
2023-03-25 17:43:33 -0500

    Added the hashmap for the buildings

--- map.js
+++ map.js
@@ -1,5 +1,30 @@
+const locationHash = new Map();
+locationHash.set("SSB", [34.7252, -86.6405]);
+locationHash.set("NCH", [34.733717, -86.640018]);
+locationHash.set("NCH",[34.733717, -86.640018]);
+locationHash.set("NUR", [34.730075, -86.638686]);
+locationHash.set("OKT", [34.719024, -86.646399]);
+locationHash.set("OPB", [34.722765, -86.638527]);
+locationHash.set("ROB", [34.730913, -86.638731]);
+locationHash.set("LIB", [34.729370, -86.639986]);
+locationHash.set("SPR", [34.731265, -86.636767]);
+locationHash.set("SST", [34.725790, -86.641162]);
+locationHash.set("UFC", [34.726598, -86.636719]);
+locationHash.set("WIL", [34.729230, -86.641442]);
+locationHash.set("BAB", [34.728245, -86.641435]);
+locationHash.set("BEV", [34.731218, -86.64289602]);
+locationHash.set("CCH", [34.730006, -86.641792]);
+locationHash.set("CTC", [34.7308, -86.641401]);
+locationHash.set("CU", [34.72713, -86.640046]);
+locationHash.set("CVA", [34.724393, -86.638828]);
+locationHash.set("CVO", [34.724692, -86.638797]);
+locationHash.set("ENG", [34.722606, -86.640502]);
+locationHash.set("FF",[34.733272, -86.640775]);
+locationHash.set("MOR",[34.733193, -86.641912]);
+locationHash.set("MSB", [34.721949, -86.638094]);
+
 var map = L.map('map').setView([34.7252, -86.6405], 14);
-var userLocation = [];
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     minZoom: 14,
@@ -89,8 +114,8 @@ var marker = L.marker([34.7252, -86.6405],
 {alt: 'UAH'}).addTo(map)
 .bindPopup('UAH');
 
-map.on('locationfound',(e)=>{
-    L.marker(e.latlng,{icon: L.icon({iconUrl: "./Assets/youAreHere.png", iconSize: [21,21]})}).addTo(map).bindPopup("Your location");
-    userLocation = e.latlng;
+map.on('locationfound',(e)=>{ //Callback for when the location is found 
+    L.marker(e.latlng,{icon: L.icon({iconUrl: "./Assets/youAreHere.png", iconSize: [21,21]})}).addTo(map).bindPopup("Your location");//Put you on the map :D
+    userLocation = e.latlng;//save the coordinates to a variable
 });
 map.on('locationerror',(e)=>alert(e.message));
