87138daa195e6de71c3aa43e2cf47626827c80a6
Nick <nbm0012@uah.edu>
2023-03-25 14:47:52 -0500

    Added callbacks to get user location and store it

--- map.js
+++ map.js
@@ -1,9 +1,21 @@
 var map = L.map('map').setView([34.7252, -86.6405], 14);
+var userLocation = [];
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     minZoom: 14,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+map.locate({setView: true,maxZoom:19});
 var marker = L.marker([34.7252, -86.6405],
 {alt: 'UAH'}).addTo(map)
 .bindPopup('UAH');
+
+map.on('locationfound',(e)=>{
+    L.marker(e.latlng).addTo(map).bindPopup("Your location");
+    userLocation = e.latlng;
+    alert(userLocation);
+});
+map.on('locationerror',(e)=>alert(e.message));
+
+
+
