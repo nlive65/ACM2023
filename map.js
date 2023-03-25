6df02323627477c1a730cba753193ef39cc9743f
Nick <nbm0012@uah.edu>
2023-03-25 14:17:19 -0500

    Fixed corrupted morton picture, streamlined map.js

--- map.js
+++ map.js
@@ -1,5 +1,9 @@
 var map = L.map('map').setView([34.7252, -86.6405], 14);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
+    minZoom: 14,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+var marker = L.marker([34.7252, -86.6405],
+{alt: 'UAH'}).addTo(map)
+.bindPopup('UAH');
