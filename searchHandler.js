6071e9957ab647cdff60c5ce07cf769b1edde633
Nick <nbm0012@uah.edu>
2023-03-26 10:30:04 -0500

    search my biggest op

--- searchHandler.js
+++ searchHandler.js
@@ -1,15 +1,12 @@
-function searchCallback() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
-    }
+input = document.getElementById("search");
+filter = input.value.toUpperCase();
+div = document.getElementById("borger");
+a = div.getElementsByTagName("a");
+for (i = 0; i < a.length; i++) {
+  txtValue = a[i].textContent || a[i].innerText;
+  if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    a[i].style.display = "";
+  } else {
+    a[i].style.display = "none";
   }
 }
